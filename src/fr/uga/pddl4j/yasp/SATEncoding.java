6ef75792078fbcdd6fc6e6ce366f0948e93f5ae5
EdenSnufkin <luca.gautiers@gmail.com>
2025-06-24 18:13:31 +0000

    First commit

---
+++ src/fr/uga/pddl4j/yasp/SATEncoding.java
@@ -0,0 +1,334 @@
+package fr.uga.pddl4j.yasp;
+
+import fr.uga.pddl4j.plan.Plan;
+import fr.uga.pddl4j.plan.SequentialPlan;
+import fr.uga.pddl4j.problem.Fluent;
+import fr.uga.pddl4j.problem.Problem;
+import fr.uga.pddl4j.problem.operator.Action;
+import fr.uga.pddl4j.util.BitVector;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * This class implements a planning problem/domain encoding into DIMACS
+ *
+ * @author H. Fiorino
+ * @version 0.1 - 30.03.2024
+ */
+public final class SATEncoding {
+    /*
+     * A SAT problem in dimacs format is a list of int list a.k.a clauses
+     */
+    private List<List<Integer>> initList = new ArrayList<List<Integer>>();
+    private int nb_fluents;
+
+    /*
+     * Goal
+     */
+    private List<Integer> goalList = new ArrayList<Integer>();
+
+    /*
+     * Actions
+     */
+    private List<List<Integer>> actionPreconditionList = new ArrayList<List<Integer>>();
+    private List<List<Integer>> actionEffectPosList = new ArrayList<List<Integer>>();
+    private List<List<Integer>> actionEffectNegList = new ArrayList<List<Integer>>();
+    private int nb_actions;
+
+    /*
+     * State transistions
+     */
+    /*
+    private HashMap<Integer, List<Integer>> addList = new HashMap<Integer, List<Integer>>();
+    private HashMap<Integer, List<Integer>> delList = new HashMap<Integer, List<Integer>>();
+    private List<List<Integer>> stateTransitionList = new ArrayList<List<Integer>>();
+    */
+
+    /*
+     * Action disjunctions
+     */
+    /*
+    private List<List<Integer>> actionDisjunctionList = new ArrayList<List<Integer>>();
+    */
+
+    /*
+     * Current DIMACS encoding of the planning domain and problem for #steps steps
+     * Contains the initial state, actions and action disjunction
+     * Goal is no there!
+     */
+    public List<List<Integer>> currentDimacs = new ArrayList<List<Integer>>();
+
+    /*
+     * Current goal encoding
+     */
+    public List<List<Integer>> currentGoal = new ArrayList<List<Integer>>();
+
+    /*
+     * Current number of steps of the SAT encoding
+     */
+    private int steps;
+
+    public SATEncoding(Problem problem, int steps) {
+
+        this.steps = steps;
+
+        // Encoding of init
+        // Each fact is a unit clause
+        // Init state step is 1
+        // We get the initial state from the planning problem
+        // State is a bit vector where the ith bit at 1 corresponds to the ith fluent being true
+        this.nb_fluents = problem.getFluents().size();
+        //System.out.println(" fluents = " + nb_fluents );
+        final BitVector init = problem.getInitialState().getPositiveFluents();
+        final List<Action> actions = problem.getActions();
+        this.nb_actions = actions.size();
+        List<Integer> clause;
+
+        // InitList => format Dimacs
+        for(Integer i=0;i<this.nb_fluents;i++){
+            clause = new ArrayList<Integer>();
+            if(init.get(i))
+            {
+                clause.add(pair(i,1));
+            }
+            else
+            {
+                clause.add(-pair(i,1));
+            }
+            clause.add(0);
+            this.initList.add(clause);
+        }
+
+        //goalList
+
+        BitVector goalBitSet = problem.getGoal().getPositiveFluents();
+        goalBitSet.or(problem.getGoal().getNegativeFluents());
+
+        for(Integer i=0;i<this.nb_fluents;i++){
+            if(goalBitSet.get(i)) {this.goalList.add(i);}
+        }
+
+        for(Integer fluent:this.goalList){
+            this.currentGoal.add(new ArrayList<Integer>(){{add(pair(fluent,1));
+            add(0);}});
+        }
+
+        //check if goal is in initList => TODO !!!
+
+        //action precondition and effect
+        BitVector actionPrecond;
+        BitVector actionEffectPos = new BitVector(nb_fluents);
+        BitVector actionEffectNeg = new BitVector(nb_fluents);
+
+        for(int i=0;i<this.nb_actions;i++){
+            actionPrecond = actions.get(i).getPrecondition().getPositiveFluents();
+
+            actionEffectNeg.clear();
+            actionEffectPos.clear();
+            
+            actionEffectPos = actions.get(i).getConditionalEffects().get(0).getEffect().getPositiveFluents(); //Only works for 1 effect and not conditionnal (condition = 0)
+            actionEffectNeg = actions.get(i).getConditionalEffects().get(0).getEffect().getNegativeFluents();
+            this.actionPreconditionList.add(new ArrayList<Integer>());
+            this.actionEffectPosList.add(new ArrayList<Integer>());
+            this.actionEffectNegList.add(new ArrayList<Integer>());
+            
+            for(int j=0;j<this.nb_fluents;j++){
+                if(actionPrecond.get(j)){this.actionPreconditionList.get(i).add(j);}
+                if(actionEffectPos.get(j)){this.actionEffectPosList.get(i).add(j);}
+                if(actionEffectNeg.get(j)){this.actionEffectNegList.get(i).add(j);}
+            }
+        }
+        
+
+        
+        encode(1, steps);
+    }
+
+    private void encode(int from, int to) {
+        this.currentDimacs.clear();
+        this.currentGoal.clear();
+        final int nb_fluents = this.nb_fluents;
+
+        //copy goal with right "final step"
+        for(Integer fluent: this.goalList){
+            this.currentGoal.add(new ArrayList<Integer>(){{
+                        add(pair(fluent,to));
+                        add(0);}});
+        }
+
+        //copy initList into dimacs
+        for( List<Integer> sublist : this.initList){
+            this.currentDimacs.add(new ArrayList<>(sublist));
+        }
+
+        
+        int action_value;
+        List<Integer> clause;
+
+        for(int curr_step=1;curr_step<to;curr_step++){
+            //an action, when applicable, has some effects
+            //(no ai or AND pi ) AND (no ai or AND ei+1)
+            for(int i=0;i<nb_actions;i++){
+                //no ai or AND pi
+                action_value= pair(i+ nb_fluents,curr_step);
+                for(Integer fluent:this.actionPreconditionList.get(i)){
+                    clause = new ArrayList<Integer>();
+                    clause.add(pair(fluent,curr_step));
+                    clause.add(-action_value);
+                    clause.add(0);
+                    this.currentDimacs.add(clause);
+                }
+                //no ai or AND ei+1
+                for(Integer fluent:this.actionEffectPosList.get(i)){
+                    clause = new ArrayList<Integer>();
+                    clause.add(-action_value);
+                    clause.add(pair(fluent,curr_step+1));
+                    clause.add(0);
+                    this.currentDimacs.add(clause);
+                }
+                for(Integer fluent:this.actionEffectNegList.get(i)){
+                    clause = new ArrayList<Integer>();
+                    clause.add(-action_value);
+                    clause.add(pair(fluent,curr_step+1));
+                    clause.add(0);
+                    this.currentDimacs.add(clause);
+                }
+
+                //no 2 actions in same step
+                //no ai or no bi
+                for(int j=0;j<this.nb_actions;j++)
+                {
+                    if(i!=j){
+                        clause = new ArrayList<Integer>();
+                        clause.add(-action_value);
+                        clause.add(-pair(j+nb_fluents,curr_step));
+                        clause.add(0);
+                        this.currentDimacs.add(clause);
+                    }
+                }
+            }
+
+            //explanatory frame axiom
+            for(int i=0;i<nb_fluents;i++){
+                //Fi OR no Fi+1 OR (OR Ai with fi in effect+ ai) AND
+                clause = new ArrayList<Integer>();
+                clause.add(pair(i,curr_step));
+                clause.add(-pair(i,curr_step+1));
+                for(int action=0;action<nb_actions;action++){
+                    if(this.actionEffectPosList.get(action).contains(i)){ 
+                        clause.add(pair(action+nb_fluents,curr_step));}
+                }
+                clause.add(0);
+                this.currentDimacs.add(clause);
+                
+                //no Fi OR Fi+1 OR (OR Ai with fi in effect- ai)
+                clause = new ArrayList<Integer>();
+                clause.add(-pair(i,curr_step));
+                clause.add(pair(i,curr_step+1));
+                for(int action=0;action<nb_actions;action++){
+                    if(this.actionEffectNegList.get(action).contains(i)){ 
+                        clause.add(pair(action+nb_fluents,curr_step));}
+                }
+                clause.add(0);
+                this.currentDimacs.add(clause);
+            }
+            
+        }
+        System.out.println("Encoding : successfully done (" + (this.currentDimacs.size()
+                + this.currentGoal.size()) + " clauses, " + to + " steps)");
+    }
+
+
+    
+    /*
+     * SAT encoding for next step
+     */
+    public void next() {
+        this.steps++;
+        encode(this.steps, this.steps);
+    }
+
+    public String toString(final List<Integer> clause, final Problem problem) {
+        final int nb_fluents = problem.getFluents().size();
+        List<Integer> dejavu = new ArrayList<Integer>();
+        String t = "[";
+        String u = "";
+        int tmp = 1;
+        int [] couple;
+        int bitnum;
+        int step;
+        for (Integer x : clause) {
+            if (x > 0) {
+                couple = unpair(x);
+                bitnum = couple[0];
+                step = couple[1];
+            } else {
+                couple = unpair(- x);
+                bitnum = - couple[0];
+                step = couple[1];
+            }
+            t = t + "(" + bitnum + ", " + step + ")";
+            t = (tmp == clause.size()) ? t + "]\n" : t + " + ";
+            tmp++;
+            final int b = Math.abs(bitnum);
+            if (!dejavu.contains(b)) {
+                dejavu.add(b);
+                u = u + b + " >> ";
+                if (nb_fluents >= b) {
+                    Fluent fluent = problem.getFluents().get(b - 1);
+                    u = u + problem.toString(fluent)  + "\n";
+                } else {
+                    u = u + problem.toShortString(problem.getActions().get(b - nb_fluents - 1)) + "\n";
+                }
+            }
+        }
+        return t + u;
+    }
+
+    public Plan extractPlan(final List<Integer> solution, final Problem problem) {
+        Plan plan = new SequentialPlan();
+        HashMap<Integer, Action> sequence = new HashMap<Integer, Action>();
+        final int nb_fluents = problem.getFluents().size();
+        int[] couple;
+        int bitnum;
+        int step;
+        for (Integer x : solution) {
+            if (x > 0) {
+                couple = unpair(x);
+                bitnum = couple[0];
+            } else {
+                couple = unpair(-x);
+                bitnum = -couple[0];
+            }
+            step = couple[1];
+            // This is a positive (asserted) action
+            if (bitnum > nb_fluents) {
+                final Action action = problem.getActions().get(bitnum - nb_fluents - 1);
+                sequence.put(step, action);
+            }
+        }
+        for (int s = sequence.keySet().size(); s > 0 ; s--) {
+            plan.add(0, sequence.get(s));
+        }
+        return plan;
+    }
+    
+    // Cantor paring function generates unique numbers
+    private static int pair(int num, int step) {
+        return (int) (0.5 * (num + step) * (num + step + 1) + step);
+    }
+
+    private static int[] unpair(int z) {
+        /*
+        Cantor unpair function is the reverse of the pairing function. It takes a single input
+        and returns the two corespoding values.
+        */
+        int t = (int) (Math.floor((Math.sqrt(8 * z + 1) - 1) / 2));
+        int bitnum = t * (t + 3) / 2 - z;
+        int step = z - t * (t + 1) / 2;
+        return new int[]{bitnum, step}; //Returning an array containing the two numbers
+    }
+
+}
---
+++ src/fr/uga/pddl4j/yasp/YetAnotherSATPlanner.java
@@ -0,0 +1,220 @@
+package fr.uga.pddl4j.yasp;
+
+import fr.uga.pddl4j.heuristics.state.FastForward;
+import fr.uga.pddl4j.parser.DefaultParsedProblem;
+import fr.uga.pddl4j.parser.ErrorManager;
+import fr.uga.pddl4j.parser.Message;
+import fr.uga.pddl4j.parser.Parser;
+import fr.uga.pddl4j.problem.DefaultProblem;
+import fr.uga.pddl4j.problem.Problem;
+import fr.uga.pddl4j.problem.State;
+import fr.uga.pddl4j.planners.statespace.AbstractStateSpacePlanner;
+import fr.uga.pddl4j.planners.statespace.HSP;
+import fr.uga.pddl4j.planners.LogLevel;
+import fr.uga.pddl4j.plan.Plan;
+
+import org.sat4j.core.Vec;
+import org.sat4j.core.VecInt;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.IProblem;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.IVecInt;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.*;
+
+/**
+ * The class shows how to use PDDL4J + SAT4J libraries to create a SAT planner.
+ *
+ * @version 0.1 - 29.03.2024
+ */
+public class YetAnotherSATPlanner extends AbstractStateSpacePlanner {
+
+    /**
+     * The main method the class. The first argument must be the path to the PDDL domain description and the second
+     * argument the path to the PDDL problem description.
+     *
+     * @param args the command line arguments.
+     */
+
+    static final int MAXSTEPS = 50;
+    // SAT solver max number of var
+    static final int MAXVAR = 1000000;
+    // SAT solver max number of clauses
+    static final int NBCLAUSES = 500000;
+    // SAT solver timeout
+    static final int TIMEOUT = 3600;
+
+    static final boolean DEBUG = false;
+
+    /**
+     * Instantiates the planning problem from a parsed problem.
+     *
+     * @param problem the problem to instantiate.
+     * @return the instantiated planning problem or null if the problem cannot be instantiated.
+     */
+    @Override
+    public Problem instantiate(DefaultParsedProblem problem) {
+        final Problem pb = new DefaultProblem(problem);
+        pb.instantiate();
+        return pb;
+    }
+
+    /**
+     * Solves the planning problem and returns the first solution found.
+     *
+     * @param problem the problem to be solved.
+     * @return a solution search or null if it does not exist.
+     */
+    @Override
+    public Plan solve(final Problem problem) {
+
+        int stepmax = MAXSTEPS;
+        Plan plan = null;
+
+        // Compute a heuristic lower bound for plan steps
+        final FastForward ff = new FastForward(problem);
+        final int hlb = ff.estimate(new State(problem.getInitialState()), problem.getGoal());
+        if (hlb > MAXSTEPS) {
+            System.out.println("Problem has no solution in " + MAXSTEPS + " steps!");
+            System.out.println("At least " + hlb + " steps are necessary.");
+            System.exit(0);
+        } else {
+
+            // Intial number of steps of the SAT encoding
+            int steps = hlb;
+
+            // Create the SAT encoding
+            SATEncoding sat = new SATEncoding(problem, steps);
+
+            // Create the SAT solver()
+            final ISolver solver = SolverFactory.newDefault();
+            solver.setTimeout(TIMEOUT);
+            // Prepare the solver to accept MAXVAR variables. MANDATORY for MAXSAT solving
+            solver.newVar(MAXVAR);
+            solver.setExpectedNumberOfClauses(NBCLAUSES);
+            IProblem ip = solver;
+            
+            boolean verbose = true;
+
+            if(verbose){
+                System.out.println("nb of actions : " + problem.getActions().size() + " / nb of fluents : " + problem.getFluents().size());
+            }
+
+
+
+
+
+            // Search starts here!
+            boolean doSearch = true;
+            List<List<Integer>> currentEncoding = new ArrayList<List<Integer>>();
+            Vec<IVecInt> VecEncoding;
+            VecInt vecIntEnco;
+            PrintWriter systemWriter = new PrintWriter(System.out);
+            
+
+            while (doSearch && !(steps > stepmax)) {
+                currentEncoding = Stream.concat(sat.currentDimacs.stream(), sat.currentGoal.stream())
+                  .collect(Collectors.toList());
+                solver.reset();
+                try{
+                    VecEncoding = new Vec<IVecInt>();
+                    for (List<Integer> clause:currentEncoding){
+                        clause.remove(new Integer(0));
+                        vecIntEnco = new VecInt(clause.stream().mapToInt(i->i).toArray());
+                        VecEncoding.push(vecIntEnco);
+                    }
+                    
+                    if(!VecEncoding.isEmpty()){solver.addAllClauses(VecEncoding);}
+                    //System.out.println(solver.toString());
+                    if(verbose){solver.printInfos(systemWriter);}
+                    doSearch = !ip.isSatisfiable();
+                }
+                catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+                
+                // doSearch = false only when solution is found
+                if (!doSearch){
+                    System.out.println("Problem is Satisfiable !");
+                    final List<Integer> solution = Arrays.stream(solver.model()).boxed().collect(Collectors.toList()); 
+                    plan = sat.extractPlan(solution,problem);
+                } else {
+                    System.out.println("Problem isn't Satisfiable :()");
+                    steps++;
+                    //Maybe do binary search on steps => possible amélioration 
+                    sat.next();
+                }
+            }
+
+            systemWriter.close();
+        }
+        return plan;
+    }
+
+    public static void main(final String[] args) {
+
+        // Checks the number of arguments from the command line
+        if (args.length != 2) {
+            System.out.println("Invalid command line");
+            return;
+        }
+
+        try {
+            // Creates an instance of the PDDL parser
+            final Parser parser = new Parser();
+            parser.setLogLevel(LogLevel.OFF);
+            // Parses the domain and the problem files.
+            final DefaultParsedProblem parsedProblem = parser.parse(args[0], args[1]);
+            // Gets the error manager of the parser
+            final ErrorManager errorManager = parser.getErrorManager();
+            // Checks if the error manager contains errors
+            if (!errorManager.isEmpty()) {
+                // Prints the errors
+                for (Message m : errorManager.getMessages()) {
+                    System.out.println(m.toString());
+                }
+            } else {
+                
+                // Creates an instance of the SAT planner
+                final YetAnotherSATPlanner planner = new YetAnotherSATPlanner();
+
+                // Prints that the domain and the problem were successfully parsed
+                System.out.print("\nparsing domain file \"" + args[0] + "\" done successfully");
+                System.out.print("\nparsing problem file \"" + args[1] + "\" done successfully\n\n");
+                
+                // Create a problem
+                final Problem problem = planner.instantiate(parsedProblem);
+
+                // Check if the goal is trivially unsatisfiable
+                if (!problem.isSolvable()) {
+                    System.out.println("Goal can be simplified to FALSE. No search will solve it");
+                    System.exit(0);
+                } else {
+                    
+                    Plan plan = planner.solve(problem);
+                        
+                        if (plan != null) {
+                            System.out.println("YetAnotherSATPlanner Plan:");
+                            System.out.println(problem.toString(plan));
+                            final AbstractStateSpacePlanner solverHSP = new HSP();
+                            try{
+                                plan = solverHSP.solve(problem);
+                                System.out.println("HSP Plan :");
+                                System.out.println(problem.toString(plan));
+                            } catch (Exception e) {e.printStackTrace();} 
+                        } else {
+                            System.out.println("No solution found!");
+                        }
+                }
+            }
+            // This exception could happen if the domain or the problem does not exist
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
